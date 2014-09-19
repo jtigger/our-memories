b8d1145b3c0be24d1c62c7c826200394fce7d699
John S. Ryan <jtigger@infosysengr.com>
2014-09-19 08:26:12 -0700

    Serves Photo.thumbnails from local filesystem (for now).

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -5,7 +5,8 @@ class Photo < ActiveRecord::Base
   end
 
   def thumbnail_url
+    #"http://dd0x0be7rjj6m.cloudfront.net/images/#{thumbnail_path}"
     #"https://s3.amazonaws.com/dotio/images/#{thumbnail_path}"
-    "http://dd0x0be7rjj6m.cloudfront.net/images/#{thumbnail_path}"
+    "#{thumbnail_path}"
   end
 end
